4f0e44ac145bfe7053bc99b8c4939172e00b6c1b
jo030225 <whwngur123@naver.com>
2021-03-14 18:27:12 +0900

    [ADD] Horizontal CollectionView

---
+++ CollectionView/CollectionView/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  CollectionView
+//
+//  Created by 조주혁 on 2021/03/14.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+}
--- CollectionView/CollectionView/ViewController.swift
+++ CollectionView/CollectionView/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var collectionView: UICollectionView! {
+        didSet {
+            collectionView.delegate = self
+            collectionView.dataSource = self
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath) as! CollectionViewCell
+        cell.clipsToBounds = true
+        cell.layer.cornerRadius = cell.frame.height * 0.5
+        cell.layer.borderWidth = 1.0
+        cell.layer.borderColor = UIColor.clear.cgColor
+        
+
+        cell.layer.shadowColor = UIColor.black.cgColor
+        cell.layer.shadowOffset = CGSize(width: 0, height: 2.0)
+        cell.layer.shadowRadius = 2.0
+        cell.layer.shadowOpacity = 0.5
+        cell.layer.masksToBounds = false
+        
+        return cell
+    }
+}
